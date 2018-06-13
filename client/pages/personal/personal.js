82a8e62db2011e77a577d5b298e28642380ad5cc
yrz1221 <yrz1221@gmail.com>
2018-06-14 00:47:34 +0800

    map by XuY

--- client/pages/personal/personal.js
+++ client/pages/personal/personal.js
@@ -13,22 +13,84 @@ Page({
     newmessage:false,
     latitude: 23.099994,
     longitude: 113.324520,
-    markers: [{
-      latitude: 23.099994,
-      longitude: 113.324520,
-      name: 'T.I.T 创意园'
-    }],
+    markers: [],
     endtime: '',
     searchLoading: true, //"上拉加载"的变量，默认true，显示  
     searchLoadingComplete: false,  //“没有更多”的变量，默认false，隐藏
     list: []
-  },
+    },
+    markertap(e) {
+    console.log(e.markerId)
+    },
+    regionchange(e) {
+    var that = this
+    that.mapCtx = wx.createMapContext("1", that)
+    that.mapCtx.getScale({
+      success: function(res){
+        if(res.scale < 13 && that.hideflag == false){
+          var tempmarkers = that.data.markers
+          that.markerstorage.push(tempmarkers[2])
+          that.markerstorage.push(tempmarkers[3])
+          that.markerstorage.push(tempmarkers[4])
+          that.markerstorage.push(tempmarkers[5])
+          tempmarkers.splice(2,4)
+          tempmarkers.push({
+            id: 2,
+            latitude: 31.02582426832666,
+            longitude: 121.43688490504894,
+            iconPath: "../../common/location.png",
+            width: 40,
+            height: 40,
+            callout: {
+              content: "上海交通大学",
+            }
+          }
+          )
+          that.setData({
+            markers: tempmarkers
+          })
+          that.hideflag = true
+        }
+        if(res.scale > 13 && that.hideflag == true){
+          var tempmarkers = that.data.markers
+          tempmarkers.pop()
+          tempmarkers.push(that.markerstorage[0])
+          tempmarkers.push(that.markerstorage[1])
+          tempmarkers.push(that.markerstorage[2])
+          tempmarkers.push(that.markerstorage[3])
+          that.markerstorage = []
+          that.setData({
+            markers: tempmarkers
+          })
+          that.hideflag = false
+        }
+        console.log(that.markerstorage)
+      }
+    })
+    },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
     var that = this;
+    wx.getLocation({
+      type:'gcj02',
+      success: function(res) {
+        that.setData({
+          latitude: res.latitude,
+          longitude: res.longitude,
+          markers:[{
+            id: 0,
+            latitude: res.latitude,
+            longitude: res.longitude,
+            callout:{
+              content: "当前位置"
+            }
+          }]
+        })
+      },
+    })
     qcloud.request({
       url: config.service.inboxupdateUrl,
       data: {
@@ -67,7 +129,7 @@ Page({
         that.setData({
           endtime: endtime
         })
-
+        var tempmarkers = that.data.markers;
         var templist = res.data;
         var textid = [];
         for (var i = 0; i < res.data.length; i++) {
@@ -81,9 +143,23 @@ Page({
           var sendtime = Y + M + D + h + m + s;
           templist[i].sendtime = sendtime;
           textid.push(templist[i].id);
+          var tempmarker={
+            id: i+1,
+            latitude: res.data[i].latitude,
+            longitude: res.data[i].longitude,
+            iconPath: "../../common/location.png",
+            width: 40,
+            height: 40,
+            callout:{
+              content: res.data[i].place_name,
+            }
+          };
+          tempmarkers.push(tempmarker)
         }
+        console.log(tempmarkers)
         that.setData({
           list: templist,
+          markers: tempmarkers,
         })
         qcloud.request({
           url: config.service.likegroupUrl,
@@ -339,5 +415,10 @@ Page({
    */
   onShareAppMessage: function () {
   
-  }
+  },
+
+  markerstorage: [
+  ],
+
+  hideflag: false
 })
